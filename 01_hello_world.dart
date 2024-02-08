0d1a1a00379fe6f4783a5c4be7966a41b75ff76d
inusui <radg21@outlook.es>
2024-02-08 00:36:11 -0500

    Hola dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+/*
+ * Comentarios 
+ */
+main(){
+  //String myName = "Inusui";
+  final myName = "Inusui";
+ 
+  
+  print("Hola $myName");
+  print("Hola con upper case ${myName.toUpperCase()}");
+  print("Contando ${ 1 + 5 }");
+}
