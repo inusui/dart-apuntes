66572c2448adbfe00badee1c6b217891ed69eab9
inusui <radg21@outlook.es>
2024-02-11 12:18:15 -0500

    Clase 1 terminada, ahora va en comentarios

--- 01_hello_world.dart
+++ 01_hello_world.dart
@@ -2,11 +2,17 @@
  * Comentarios 
  */
 main(){
-  //String myName = "Inusui";
+
+}
+/**
+ * Clase 1 
+ * 
+ *   //String myName = "Inusui";
   final myName = "Inusui";
  
   
   print("Hola $myName");
   print("Hola con upper case ${myName.toUpperCase()}");
   print("Contando ${ 1 + 5 }");
-}
+
+ */
